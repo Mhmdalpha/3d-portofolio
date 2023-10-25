d536928c06659fa1dbbadc56fa428f164c6d481c
Muhammad Alpha <122379779+Mhmdalpha@users.noreply.github.com>
2023-10-25 19:59:35 +0700

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -90,7 +90,7 @@ const Works = () => {
          </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-1.5">
+      <div className="mt-20 flex flex-wrap gap-1">
         {projects.map((project,index)=> (
           <ProjectCard 
            key={`project-${index}`}
