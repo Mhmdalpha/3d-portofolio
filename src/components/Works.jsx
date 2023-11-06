2baab1d26db1d1c6f1c3688f152fe20c4fbd9ee4
Muhammad Alpha <122379779+Mhmdalpha@users.noreply.github.com>
2023-11-06 10:13:00 +0700

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,7 +9,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 
 const ProjectCard = ({ index, name, description, tags, 
-  image, source_code_Link }) => {
+  image, source_code_Link, Link_to_project }) => {
     return (
        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
         <Tilt
